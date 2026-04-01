9a2caf4adc5f33491bfadcb1154501881c256155
FacundoAguilar <faguilar@alumnos.unsada.edu.ar>
2026-04-01 20:47:44 -0300

    Ejercicio-2

--- Actividad-3/Modulo-1/Modulo-5/Ejercicio-2.py
+++ Actividad-3/Modulo-1/Modulo-5/Ejercicio-2.py
@@ -1,2 +1,22 @@
 #2. Escribir un programa que solicite el ingreso de un número entero e informe si el
 #valor leído es o no mayor que el número 10.
+
+def solicitar_numero(mensaje):
+    "Solicitar numero al usuario"
+    while True:
+        try:
+            return int(input(mensaje))
+        except ValueError:
+            print("Entrada inválidad, ingresar un número entero")
+
+num1 = solicitar_numero("Ingrsar el primero número: ")
+num10= 10
+
+if (num1 > num10):
+    print("El número: ", num1 , " es mayor al número ", num10)
+else:
+    if (num10 > num1):
+        print("El número: ", num1 , "es menor al número: ", num10)
+    else:
+        if (num1 == num10):
+            print("El número: ", num1, " es igual número: ", num10)
