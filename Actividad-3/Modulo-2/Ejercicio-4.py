74700f22da740d70bc685f37219cd9597033e29a
THEfoxde <thefoxde2005@gmail.com>
2026-04-01 18:50:35 -0300

    Update Ejercicio-4.py

--- Actividad-3/Modulo-2/Ejercicio-4.py
+++ Actividad-3/Modulo-2/Ejercicio-4.py
@@ -1,3 +1,20 @@
 #4 Escribir un programa que solicite el ingreso de un número entero y, si el número
 #leído es par, imprima la leyenda 'El número es PAR'. En caso contrario, deberá
 #mostrar el texto 'El número es IMPAR'.
+
+
+
+numero = int(input("Ingrese un número entero: "))
+
+try:
+    numero = int(numero)
+except ValueError:
+    print("El valor ingresado no es un número entero válido.")
+
+
+if numero % 2 == 0:
+    print("El número es PAR")
+else:
+    print("El número es IMPAR")
+
+print ("vamos los pibes")
