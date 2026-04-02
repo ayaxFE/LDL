d438f495eda8b188816167d2c0d309dcf38ef6e5
Luis M. Díaz <84762039+lmdiaz-dv@users.noreply.github.com>
2026-04-02 20:07:49 -0300

    Update Ejercicio-10.py

--- Actividad-3/Modulo-5/Ejercicio-10.py
+++ Actividad-3/Modulo-5/Ejercicio-10.py
@@ -2,3 +2,28 @@
 #encuentren entre -10 y 10 e imprima la sumatoria de los valores negativos, la
 #cantidad de valores iguales a cero y el promedio de los valores positivos. Se
 #deberá pedir el reingreso de un número si éste estuviera fuera del rango dado.
+
+lista_numeros = []
+sumatoria_negativos = 0
+
+for x in range(8):
+    numero = int(input("Ingrese un número entre -10 y 10: "))
+    while numero not in range(-11, 11):
+        numero = int(input("El número no se encuentra entre -10 y 10. Ingrese otro: "))
+    lista_numeros.append(numero)
+print(f"Lista de números: {lista_numeros}")
+
+print("La cantidad de ceros ingresados es de: ", lista_numeros.count(0))
+
+for x in lista_numeros:
+    if x < 0:
+        sumatoria_negativos += x
+
+print(f"Sumatoria de números negativos: {sumatoria_negativos}")
+
+valores_positivos = [x for x in lista_numeros if x > 0]
+if valores_positivos:
+    promedio = sum(valores_positivos) / len(valores_positivos)
+    print(f"Promedio de positivos: {promedio}")
+else:
+    print("No se encontraron números positivos.")
