92363c183fd37df6fbad946c22fd18195e755c0a
FacuDeRenzo <127690457+FacuDeRenzo@users.noreply.github.com>
2026-04-03 20:18:14 -0300

    ejercicio 8 realizado

--- Actividad-3/Modulo-4/Ejercicio-8.py
+++ Actividad-3/Modulo-4/Ejercicio-8.py
@@ -2,3 +2,13 @@
 #procesar. Luego, permitir al usuario ir ingresando uno a uno la cantidad pedida
 #de números. Una vez finalizado el ingreso, se deberá mostrar la suma total de los
 #números ingresados.
+
+total_num = int(input("Ingrese la cantidad de numeros a procesar: "))
+numeros = 0
+sum_total = 0
+
+while numeros < total_num:
+    numeros = int(input("Ingrese un numero: "))
+    sum_total += numeros
+    
+print("La suma de todos los numeros es: ", sum_total)    
