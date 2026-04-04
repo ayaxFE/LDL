fbe29a0f64fd708de8644d85105722d149fa1612
BrunooGenz <bgenz@alumnos.unsada.edu.ar>
2026-04-04 17:52:29 -0300

    Ejercicio-6.py

--- Actividad-3/Modulo-3/Ejercicio-6.py
+++ Actividad-3/Modulo-3/Ejercicio-6.py
@@ -1,3 +1,10 @@
 #6.Solicitar el ingreso de un número entero e imprimir los números correlativos
 #desde el ingresado hasta el doble del mismo. Por ejemplo: si se ingresa un 6, se
 #deberá mostrar: 6, 7, 8, 9, 10, 11, 12.
+
+numero = int(input("ingrese un numero:"))
+doble = numero*2
+
+while (numero <= doble):
+    print (numero)
+    numero = numero+1
