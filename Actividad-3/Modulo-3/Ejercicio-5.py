d7ea5b9e1f855d0c65eaceebb3f90a2bfcc558dc
BrunooGenz <bgenz@alumnos.unsada.edu.ar>
2026-04-04 17:51:45 -0300

    Ejercicio-5.py

--- Actividad-3/Modulo-3/Ejercicio-5.py
+++ Actividad-3/Modulo-3/Ejercicio-5.py
@@ -2,3 +2,9 @@
 #comprados por un cliente en un comercio. Finalmente, imprimir “accede al
 #descuento” si el día es lunes y el cliente compró más de 3 artículos. En caso
 #contrario, no imprimir nada.
+
+dia = str(input("ingrese un dia:").strip().lower())
+cantidadArticulos = int(input("ingrese la cantidad de articulos:"))
+
+if (dia=="lunes") and (cantidadArticulos>3):
+    print ("Accede al descuento")
